aaf3dfe662f11d774342671224d333f6ab53c2ff
vasker <yarinvasket@gmail.com>
2022-06-06 14:15:02 +0300

    add comments

--- client-api.ts
+++ client-api.ts
@@ -13,7 +13,8 @@ function base64decode(base64str: string) {
     return Buffer.from(base64str, 'base64').toString('utf8');
 }
 
-async function register(username : string, password : string) {
+async function register(username: string, password: string) {
+//  Generate key pair
     const { privateKey, publicKey } = await openpgp.generateKey({
         type: 'rsa', // Type of the key
         rsaBits: 4096, // RSA key size (defaults to 4096 bits)
@@ -21,6 +22,7 @@ async function register(username : string, password : string) {
         format: 'object' // output key format, defaults to 'armored'
     });
 
+//  Prove ownership of public key by signing username + globalsalt
     const message = await openpgp.createMessage({text: username + globalsalt});
     const signature = await openpgp.sign({
         message,
@@ -28,6 +30,7 @@ async function register(username : string, password : string) {
         detached: true
     });
 
+//  Encryption key for the private key
     const salt2 = await bcrypt.genSalt();
     const encryptionKey = await bcrypt.hash(password, salt2);
 
@@ -35,14 +38,17 @@ async function register(username : string, password : string) {
         text: privateKey.armor()
     });
 
+//  Encrypt private key
     const encrypted_private_key = await openpgp.encrypt({
         message: privateKeyMessage,
         passwords: encryptionKey
     });
 
+//  Hash password to prevent the server knowing it
     const hashed_password = await bcrypt.hash(password,
         await bcrypt.genSalt());
 
+//  Prepare to send the request
     const requestBody = {
         user_name: username,
         public_key: base64encode(publicKey.armor()),
@@ -51,7 +57,8 @@ async function register(username : string, password : string) {
         hashed_password,
         salt2
     };
-    
+
+//  Send the request
     const registerResponse = await fetch(
         serverurl + '/register', {
             method: 'POST',
